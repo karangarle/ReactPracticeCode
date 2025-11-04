346cc7757cab91352fde6919a398e7f0edf0d804
karan <karangarale123@gmail.com>
2025-11-04 23:33:19 +0530

    add propes in card using array of object

--- card-design_30-10/src/components/Card.jsx
+++ card-design_30-10/src/components/Card.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { Icon } from "@iconify/react";
 
-function Card() {
+function Card(props) {
   return (
     <div>
       <div className="card">
@@ -9,7 +9,7 @@ function Card() {
           <div className="circle-image">
             <img
               className="image"
-              src="https://i.pinimg.com/1200x/0a/06/60/0a06600cc3cedeb49280b54114c88ce6.jpg"
+              src={props.img}
             />
           </div>
           <div className="save-tag">
@@ -24,22 +24,22 @@ function Card() {
         <div className="card-content">
           <div className="title-content">
             <h3>
-              Amazon <span>5 days Ago</span>
+              {props.name} <span>{props.postAt}</span>
             </h3>
           </div>
           <div className="des-content">
-            <h2>Senior ui/ux Designer</h2>
+            <h2>{props.jobTitle}</h2>
           </div>
           <div className="tag">
-            <h5>Part Time</h5>
-            <h5>Senior Level</h5>
+            <h5>{props.tagPrimary}</h5>
+            <h5>{props.tagSecondary}</h5>
           </div>
         </div>
         <hr />
         <div className="card-bottom">
           <div className="Left-Side">
-            <p>150-220k</p>
-            <span>Kochi,India</span>
+            <p>{props.salaryRange}</p>
+            <span>{props.location}</span>
           </div>
           <div className="right-side">
             <button type="submit">Apply Now</button>
